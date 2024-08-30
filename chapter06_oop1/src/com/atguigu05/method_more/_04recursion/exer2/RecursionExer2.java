02faaeb28c8c02e78628cf27cd7d534cb14af0e8
HerryWEu <yihong.wu@mhp.com>
2024-08-30 18:23:49 +0800

    feat: 测试递归

---
+++ chapter06_oop1/src/com/atguigu05/method_more/_04recursion/exer2/RecursionExer2.java
@@ -0,0 +1,16 @@
+package com.atguigu05.method_more._04recursion.exer2;
+
+/**
+ * ClassName: RecursionTest
+ * Package: com.atguigu05.method_more._04recursion.exer2
+ * Description:
+ *
+ * @Author Xu, Luqin
+ * @Create 2024/8/30 15:21
+ * @Version 1.0
+ */
+public class RecursionExer2 {
+    public static void main(String[] args) {
+        RecursionExer2 exer2 = new RecursionExer2();
+    }
+}
