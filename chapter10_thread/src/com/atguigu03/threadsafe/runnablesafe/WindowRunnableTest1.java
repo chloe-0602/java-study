4ca77d6eda2c599acd92f3ec15f25fb121da3f66
HerryWEu <yihong.wu@mhp.com>
2024-09-04 10:07:03 +0800

    feat: 多线程 - 使用synchronized 通过方法 实现线程 安全，注意在继承Thread的时候同步监视器 的唯一性问题

--- chapter10_thread/src/com/atguigu03/threadsafe/runnablesafe/WindowRunnableTest1.java
+++ chapter10_thread/src/com/atguigu03/threadsafe/runnablesafe/WindowRunnableTest1.java
@@ -4,14 +4,14 @@ package com.atguigu03.threadsafe.runnablesafe;
  * ClassName: WindowRunnableTest
  * Package: com.atguigu03.threadsafe.notsafe
  * Description:
- *    使用synchronized实现同步代码块 线程安全
+ *   使用synchronized 实现方法的线程安全
  * @Author Xu, Luqin
  * @Create 2024/9/4 9:26
  * @Version 1.0
  */
-public class WindowRunnableTest {
+public class WindowRunnableTest1 {
     public static void main(String[] args) {
-        WindowRunnable runnable = new WindowRunnable();
+        WindowRunnable1 runnable = new WindowRunnable1();
         Thread t1 = new Thread(runnable);
         Thread t2 = new Thread(runnable);
         Thread t3 = new Thread(runnable);
@@ -26,20 +26,23 @@ public class WindowRunnableTest {
     }
 }
 
-class WindowRunnable implements Runnable{
+class WindowRunnable1 implements Runnable{
     private int ticket = 100;
+    private boolean isFlag = true;
     @Override
     public void run() {
-        while (true){
-            synchronized (this) {//唯一吗？
-                if(ticket>0){
+        while (isFlag){
+            saleTicket();
+        }
+    }
 
-                    System.out.println(Thread.currentThread().getName()+ "卖出：" + ticket);
-                    ticket--;
-                }else{
-                    break;
-                }
-            }
+    public synchronized void saleTicket(){ // 此时默认的同步监视器是this
+        if(ticket>0){
+
+            System.out.println(Thread.currentThread().getName()+ "卖出：" + ticket);
+            ticket--;
+        }else{
+            isFlag=false;
         }
     }
 }
---
+++ chapter10_thread/src/com/atguigu03/threadsafe/threadsafe/WindowThreadTest1.java
@@ -0,0 +1,47 @@
+package com.atguigu03.threadsafe.threadsafe;
+
+/**
+ * ClassName: WindowThreadTest
+ * Package: com.atguigu03.threadsafe.notsafe
+ * Description:
+ *      使用synchronized 实现方法的线程安全
+ * @Author Xu, Luqin
+ * @Create 2024/9/4 9:40
+ * @Version 1.0
+ */
+public class WindowThreadTest1 {
+    public static void main(String[] args) {
+        WindowThread1 w1 = new WindowThread1();
+        WindowThread1 w2 = new WindowThread1();
+        WindowThread1 w3 = new WindowThread1();
+
+        w1.setName("窗口1");
+        w2.setName("窗口2");
+        w3.setName("窗口3");
+
+        w1.start();
+        w2.start();
+        w3.start();
+    }
+}
+
+class WindowThread1 extends Thread{
+    private static int ticket = 100;
+    private static boolean isFlag= true;
+    @Override
+    public void run() {
+        while (isFlag){
+            saleTicket();
+        }
+    }
+
+//    public synchronized void saleTicket(){ // 此时的 同步监视器是 this. !!!!线程不安全
+    public synchronized static void saleTicket(){ // 此时的 同步监视器是 WindowThread1.class 唯一线程安全
+        if(ticket>0){
+            System.out.println(Thread.currentThread().getName()+ "卖出：" + ticket);
+            ticket--;
+        }else{
+            isFlag = false;
+        }
+    }
+}
