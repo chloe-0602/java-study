8ce1cd449d8716931db23b1d8ce764e5933df6cc
HerryWEu <yihong.wu@mhp.com>
2024-09-07 15:47:04 +0800

    feat: 数据结构 - 源码

--- chapter14_datastructure_teacher/src/com/atguigu02/list/ListTest.java
+++ chapter14_datastructure_teacher/src/com/atguigu02/list/ListTest.java
@@ -17,22 +17,26 @@ import java.util.Vector;
 public class ListTest {
 
     @Test
-    public void test1(){
+    public void test1() {
         ArrayList<String> list = new ArrayList<>();
 
-        list.add("AA"); //elementData[0] = "AA";
+        for (int i = 0; i < 10; i++) {
+            list.add("AA"); //elementData[0] = "AA";
+        }
         list.add("BB");//elementData[1] = "BB";
     }
 
     @Test
-    public void test2(){
+    public void test2() {
         Vector v = new Vector(); //底层初始化数组，长度为10.Object[] elementData = new Object[10];
-        v.add("AA"); //elementData[0] = "AA";
+        for (int i = 0; i < 10; i++) {
+            v.add("AA"); //elementData[0] = "AA";
+        }
         v.add("BB");//elementData[1] = "BB";
     }
 
     @Test
-    public void test3(){
+    public void test3() {
         LinkedList<String> list = new LinkedList<>(); //底层也没做啥
         list.add("AA"); //将"AA"封装到一个Node对象1中，list对象的属性first、last都指向此Node对象1。
         list.add("BB"); //将"BB"封装到一个Node对象2中，对象1和对象2构成一个双向链表，同时last指向此Node对象2
