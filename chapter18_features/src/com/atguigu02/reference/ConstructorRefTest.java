669b32d63a30f0dbe8592e5547277fe8dee82e7b
HerryWEu <yihong.wu@mhp.com>
2024-09-09 20:16:52 +0800

    feat: JDK8-17新特性 - 构造器引用和方法引用练习

--- chapter18_features/src/com/atguigu02/reference/ConstructorRefTest.java
+++ chapter18_features/src/com/atguigu02/reference/ConstructorRefTest.java
@@ -1,6 +1,11 @@
 package com.atguigu02.reference;
 
+import com.atguigu02.reference.data.Employee;
 import org.junit.Test;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 /**
  * 一、构造器引用
@@ -15,25 +20,54 @@ public class ConstructorRefTest {
     //Supplier中的T get()
     @Test
     public void test1(){
+        Supplier<Employee> sup1 = new Supplier<Employee>() {
+            @Override
+            public Employee get() {
+                return new Employee();
+            }
+        };
+        System.out.println(sup1.get());
 
-	}
+        System.out.println();
+
+        Supplier<Employee> sup2 = Employee::new;
+        System.out.println(sup2.get());
+
+    }
 
 	//Function中的R apply(T t)
     @Test
     public void test2(){
-
-	}
+        Function<Integer, Employee> func = Employee::new;
+        Employee employee = func.apply(2);
+        System.out.println(employee);
+    }
 
 	//BiFunction中的R apply(T t,U u)
     @Test
     public void test3(){
-
-	}
+        BiFunction<Integer, String, Employee> biFunction = Employee::new;
+        Employee employee = biFunction.apply(3, "Jerry");
+        System.out.println(employee);
+    }
 
 	//数组引用
     //Function中的R apply(T t)
     @Test
     public void test4(){
+        Function<Integer, Employee[]>  func = new Function<Integer, Employee[]>() {
+            @Override
+            public Employee[] apply(Integer num) {
+                return new Employee[num];
+            }
+        };
+        Employee[] employees = func.apply(10);
+        System.out.println(employees.length);
 
-	}
+        System.out.println("==========");
+
+        Function<Integer, Employee[]> func1 = Employee[]::new;
+        Employee[] employees1 = func1.apply(2);
+        System.out.println(employees1.length);
+    }
 }
--- chapter18_features/src/com/atguigu02/reference/MethodRefTest.java
+++ chapter18_features/src/com/atguigu02/reference/MethodRefTest.java
@@ -1,6 +1,13 @@
 package com.atguigu02.reference;
 
+import com.atguigu02.reference.data.Employee;
 import org.junit.Test;
+
+import java.util.Comparator;
+import java.util.function.BiPredicate;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 /**
  * 方法引用的使用
@@ -14,16 +21,27 @@ public class MethodRefTest {
 	//PrintStream中的void println(T t)
 	@Test
 	public void test1() {
-		
-		
+		Consumer<String> con1 = System.out::println;
+		con1.accept("this is accept");
 	}
 	
 	//Supplier中的T get()
 	//Employee中的String getName()
 	@Test
 	public void test2() {
-		
-		
+		Employee emp = new Employee(1, "Tom");
+		Supplier<String> sup1 = new Supplier<String>() {
+			@Override
+			public String get() {
+				return emp.getName();
+			}
+		};
+
+		System.out.println(sup1.get());
+
+		System.out.println("---------------------");
+		Supplier<String> sup2 = emp::getName;
+		System.out.println(sup2.get());
 	}
 
 	// 情况二：类 :: 静态方法
@@ -31,15 +49,17 @@ public class MethodRefTest {
 	//Integer中的int compare(T t1,T t2)
 	@Test
 	public void test3() {
-		
+		Comparator<Integer> com1 = Integer::compare;
+		System.out.println(com1.compare(21, 22));
 	}
 	
 	//Function中的R apply(T t)
 	//Math中的Long round(Double d)
 	@Test
 	public void test4() {
-		
-
+		Function<Double, Long> fun1 = Math::round;
+		Long res = fun1.apply(12.3333);
+		System.out.println(res);
 	}
 
 	// 情况三：类 :: 实例方法 
@@ -47,22 +67,29 @@ public class MethodRefTest {
 	// String中的int t1.compareTo(t2)
 	@Test
 	public void test5() {
-		
+		Comparator<String> com1 = String::compareTo;
+		System.out.println(com1.compare("ha", "ha"));
+
 	}
 
 	//BiPredicate中的boolean test(T t1, T t2);
 	//String中的boolean t1.equals(t2)
 	@Test
 	public void test6() {
-		
-		
+		BiPredicate<String, String> bip1 = String::equals;
+		System.out.println(bip1.test("21x1", "1"));
+
 	}
 	
 	// Function中的R apply(T t)
 	// Employee中的String getName();
 	@Test
 	public void test7() {
-		
+		Function<Employee, String> func1 = Employee::getName;
+
+		Employee employee = new Employee(1, "Jerry");
+		String res = func1.apply(employee);
+		System.out.println(res);
 	}
 
 }
